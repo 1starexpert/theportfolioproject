d230abed4cfe4337428626ff31ff36f161e5b2e7
Johnny <52709518+1starexpert@users.noreply.github.com>
2025-08-18 09:17:59 -0400

    Add files via upload

---
+++ yore/pages/1_decision.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import info3 as info
+import pandas as pd
+
+
+st.title("Decisision!!!!")
+
+st.write("""
+
+I have just woke up and had a sudden idea on what I want my first personal
+project to be. I want to create my own Legend Tracker for Clash of Clans.
+Legend Trackers already exist, but they tend to be inaccurate at times.
+
+""")
+
+st.subheader("What is Legends League? And Why Do We Need Trackers?")
+
+st.write("""
+
+To casual players, Legends League is understood to be essentially Clash's
+"ranked" mode. It's the most competitive league/division in the game.
+One needs to have above 5000 trophies (Clash's elo system) in order to enter.
+
+However, to the top .01% of Legends League players, Legends League is
+essentially a business. At times, it can even get quite shady.
+
+Officially, people can make money from playing Clash of Clans in two ways:
+
+1) Official tournaments, such as the World Championships with a prize pool
+of one million dollars.
+
+2) Content creation such as Youtube and Twitch.
+
+This is pretty standard for most video games.
+
+However, there is an even shadier side, one tends to be frowned upon
+by the developers of the game.
+
+Unofficially, there are three main ways people make money:
+
+1) "Clan Mercenaries:" These are high trophy players that Clan leaders
+would pay to join their clan for the month. This is done to boost their Clan
+Ranking on the leaderboards and help their clan gain recognition to attract
+more players. 
+
+
+2) Name changes. Players in the top 5-10 of the World leaderboards
+are given money to change their name temporarily for the month.
+
+This can be anywhere from 200 to 1000 dollars per account. Remember many of
+these guys are playing like 10+ accounts.
+
+3) "Pilotting." High level players are paid to play the accounts of other
+players, helping them finish higher in the World rankings at the end of every
+month. Funny story, Elon Musk has been exposed for doing this with his
+Diablo account.
+
+Because of these lucrative ways to make money unoffically, it has recently
+given rise to another lucrative form of business: Subscription services!!!!
+
+Many players want to get better at the game. They want to be able to rise to the
+top of the leaderboard and maybe make some money on the side. 
+
+""")
+
