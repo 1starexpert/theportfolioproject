4526cde43cd6ed0eda87a78744a26665448c3f94
Johnny <52709518+1starexpert@users.noreply.github.com>
2025-12-01 12:25:42 -0500

    Update clan_frontend.py

--- clash_of_clans/clan_frontend.py
+++ clash_of_clans/clan_frontend.py
@@ -1,6 +1,13 @@
-import clan_backend
+#import clan_backend
 print("Backend imported successfully!")
 
 import requests
 
 import streamlit as st
+
+
+
+def textbox1():
+
+    clan_tag = st.text_input("Enter a clan tag:")
+    return clan_tag
